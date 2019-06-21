f7556a4ff5eb27b5ffc83c17d6540d582e89df92
jlbokass <j_dabok@me.com>
2019-06-21 05:59:35 +0200

    Pagination product OK

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Representation\Products;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,19 +28,49 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Rest\Get(
-     *     path="/products",
-     *     name="app_product_list"
+     * @param ParamFetcherInterface $paramFetcher
+     *
+     * @return
+     *
+     * @Rest\Get("/products", name="app_product_list")
+     * @Rest\QueryParam(
+     *     name="keyword",
+     *     requirements="[a-zA-Z0-9]",
+     *     nullable=true,
+     *     description="The keyword to search for."
+     * )
+     * @Rest\QueryParam(
+     *     name="order",
+     *     requirements="asc|desc",
+     *     default="asc",
+     *     description="Sort order (asc or desc)"
+     * )
+     * @Rest\QueryParam(
+     *     name="limit",
+     *     requirements="\d+",
+     *     default="10",
+     *     description="Max number of article per page."
+     * )
+     * @Rest\QueryParam(
+     *     name="offset",
+     *     requirements="\d+",
+     *     default="0",
+     *     description="The pagination offset"
      * )
      *
      * @Rest\View(
      *     statusCode=200
      * )
-     *
-     * @Rest\QueryParam(name="order")
      */
-    public function list($order)
+    public function list(ParamFetcherInterface $paramFetcher)
     {
-        dd($order);
+        $pager = $this->getDoctrine()->getRepository(Product::class)->search(
+            $paramFetcher->get('keyword'),
+            $paramFetcher->get('order'),
+            $paramFetcher->get('limit'),
+            $paramFetcher->get('offset')
+        );
+
+        return new Products($pager);
     }
 }
