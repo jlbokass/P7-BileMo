6e139316c3b02db1bba8e4837182ddd7cc897d54
jlbokass <j_dabok@me.com>
2019-06-25 13:11:41 +0200

    WIP

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,6 +8,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Nelmio\ApiDocBundle\Annotation as Doc;
 use Nelmio\ApiDocBundle\Annotation\Security;
