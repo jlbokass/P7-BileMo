9e4ac771b8ab3f27c4d548b61c5072859c70d010
jlbokass <j_dabok@me.com>
2019-06-30 00:59:31 +0200

    Updated the readme

--- public/graph.php
+++ public/graph.php
@@ -1,12 +1,12 @@
 <?php
 $debut = microtime(true);
 
-for ($i=0; $i < 10; $i++) {
+for ($i=0; $i < 1000; $i++) {
     echo 'Hello world';
 }
-
+echo "<br/>";
 $fin = microtime(true);
 
 $delai = $fin - $debut;
 
-echo 'Temps ecloue: ' . $delai . 'millisecondes.';
+echo 'Temps ecoule: ' . $delai . ' millisecondes.';
